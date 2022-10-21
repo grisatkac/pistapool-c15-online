4806a8f24d8a8f9c086a61e8e7c284cd371fb477
grisatkac <36537579+grisatkac@users.noreply.github.com>
2022-10-21 11:45:15 +0300

    add fields in SecondBranch class (#3)
    * add fields in SecondBranch class
    
    * add getters and setters in SecondBranch class

--- src/com/tms/git/second_branch/SecondBranch.java
+++ src/com/tms/git/second_branch/SecondBranch.java
@@ -1,4 +1,23 @@
 package com.tms.git.second_branch;
 
 public class SecondBranch {
+
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
